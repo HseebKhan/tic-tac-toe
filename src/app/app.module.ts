9b347b548a75e07b38f76e883da80c28be0b6423
unknown <abdul.haseeb2097@gmail.com>
2020-09-16 17:47:27 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from "./board/board.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NbThemeModule, NbLayoutModule, NbButtonModule } from "@nebular/theme";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SquareComponent, BoardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: "cosmic" }),
+    NbLayoutModule,
+    NbButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
